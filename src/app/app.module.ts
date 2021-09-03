486d5764e9ed5a57108c0da1dba499ee8f99a02c
Rafael <rafael.soares@sigtrans.com.br>
2021-09-03 16:02:05 -0300

    Feature: inicializado o repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ConfirmationComponent } from './pages/signup/confirmation/confirmation.component';
+import { CollaboratorsComponent } from './pages/collaborators/collaborators.component';
+import { FiltrosComponent } from './pages/collaborators/filtros/filtros.component';
+import { NewCollaboratorsComponent } from './pages/collaborators/new-collaborators/new-collaborators.component';
+import { EditCollaboratorComponent } from './pages/collaborators/edit-collaborator/edit-collaborator.component';
+import { ProfileComponent } from './pages/collaborators/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ConfirmationComponent,
+    CollaboratorsComponent,
+    FiltrosComponent,
+    NewCollaboratorsComponent,
+    EditCollaboratorComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/collaborators/edit-collaborator/edit-collaborator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-collaborator',
+  templateUrl: './edit-collaborator.component.html',
+  styleUrls: ['./edit-collaborator.component.css']
+})
+export class EditCollaboratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
